37667a5cbde6f2f70570f27225cf6bace899b165
foultrip <davidvasquezmahecha@gmail.com>
2024-05-27 18:44:24 -0500

    act001

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  const completedTasks = {};
+  const tasks = JSON.parse(body);
+
+  for (const task of tasks) {
+    if (task.completed) {
+      if (!completedTasks[task.userId]) {
+        completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId]++;
+      }
+    }
+  }
+  console.log(completedTasks);
+});
