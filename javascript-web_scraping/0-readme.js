3879590eb991c06d1330641ebe227c9756092e99
foultrip <davidvasquezmahecha@gmail.com>
2024-05-29 01:41:33 -0500

    little change

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const fs = require("fs");
+const filePath = process.argv[2];
 
-const file = process.argv[2];
-
-fs.readFile(file, "utf-8", (err, data) => {
+fs.readFile(filePath, "utf-8", (err, data) => {
   if (err) {
     console.error(err);
   } else {
